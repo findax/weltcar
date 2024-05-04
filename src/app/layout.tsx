27b5902a46c58d57fc455b9cc8ab5b33d51015ac
BuzRom <romanbuzukov@gmail.com>
2024-05-04 12:22:56 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,34 @@
 import { Poppins } from 'next/font/google';
 import LoadingProgressBar from '@/components/LoadingProgressBar';
 import Header from '../components/header/Header';
+import Footer from '@/components/Footer';
+import { Metadata, Viewport } from 'next';
+
 import './globals.css';
 import '@/fonts/line-awesome-1.3.0/css/line-awesome.css';
 import '@/styles/index.scss';
 import 'rc-slider/assets/index.css';
-import Footer from '@/components/Footer';
 
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
   weight: ['300', '400', '500', '600', '700'],
 });
+
+export const metadata: Metadata = {
+  title: 'WeltCar - premium automobiles',
+  description:
+    'Explore our curated selection of premium automobiles, where every ride embodies elegance and sophistication',
+  keywords: 'Cars, Vehicles, Automobiles',
+};
+
+export const viewport: Viewport = {
+  themeColor: 'white',
+  // width: 'device-width',
+  // initialScale: 1,
+  // maximumScale: 1,
+  // userScalable: false,
+};
 
 export default function RootLayout({
   children,
