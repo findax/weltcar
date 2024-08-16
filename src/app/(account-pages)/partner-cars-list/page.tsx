382ff5c88513d76dfd54b576ea8214e63247a1e8
EvgeniyTyupa <boogee2820@gmail.com>
2024-08-16 15:14:07 +0300

    change partner check for button

--- src/app/(account-pages)/partner-cars-list/page.tsx
+++ src/app/(account-pages)/partner-cars-list/page.tsx
@@ -4,22 +4,24 @@ import { Suspense, useEffect, useState } from 'react';
 import { CatalogPartner } from './(components)/CatalogPartner';
 import { ProtectedRoute, UserRole } from '@/utils/protectedRoute';
 import LoadingSpinner from '@/shared/LoadingSpinner';
-import { ICarsPartner } from '@/types/partner';
+import { ICarsPartner, IPartnerResponse } from '@/types/partner';
 import { getPartnerCars } from '@/api/cars';
 import ErrorComponent from '@/components/ErrorComponent';
 import { ButtonPrimary } from '@/shared/Buttons';
+import { getPartner } from '@/api/partner';
 
 
 const PartnerCarsListPage = () => {
   const [isFirstLoading, setFirstLoading] = useState(true);
   const [isError, setError] = useState(false)
   const [carListData, setCarListData] = useState<ICarsPartner[]>([]);
+  const [partner, setPartner] = useState<IPartnerResponse>();
 
   useEffect(() => {
     if(isFirstLoading){
-      getPartnerCars(1,10)
-        .then((data) => {
-          data && setCarListData(data);
+      Promise.all(([getPartner(), getPartnerCars(1, 10)]))
+        .then(([partner, carListdata]) => {
+          partner && carListdata && (setPartner(partner), setCarListData(carListdata));
         })
         .finally(() => {
           isFirstLoading && setFirstLoading(false);
@@ -59,9 +61,12 @@ const PartnerCarsListPage = () => {
           : (
               <div className='h-[40vh] flex justify-center items-center flex-col bg-white/50 dark:bg-neutral-800/60'>
                 <h3 className='text-2xl'>You have no added the cars</h3>
-                <ButtonPrimary className='mt-6' href='/partner-cars'>
-                  Add car
-                </ButtonPrimary>
+                {
+                  partner?.is_verified && 
+                  <ButtonPrimary className='mt-6' href='/partner-cars'>
+                    Add car
+                  </ButtonPrimary>
+                }
               </div>
             )
         )
