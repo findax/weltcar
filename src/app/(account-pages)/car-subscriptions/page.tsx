a12ba2038c4af4ed956c572074f805cd6a527dc0
EvgeniyTyupa <boogee2820@gmail.com>
2024-09-12 11:35:52 +0300

    add description and remove tag on backspace

--- src/app/(account-pages)/car-subscriptions/page.tsx
+++ src/app/(account-pages)/car-subscriptions/page.tsx
@@ -2,13 +2,12 @@
 
 import CarSelector from "@/components/CarSelector";
 import { Suspense } from "react";
+import CarSubscriptions from "./(components)/CarSubscriptions";
 
 const CarSubscribtionsPage = () => {
   return (
     <div className='relative space-y-6 md:space-y-8 lg:min-h-[650px]'>
-      <Suspense>
-        <CarSelector />
-      </Suspense>
+      <CarSubscriptions />
     </div>
   );
 };
