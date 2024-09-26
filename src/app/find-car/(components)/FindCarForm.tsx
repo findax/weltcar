1384985b57cfc1f248481e004083eeecadc1ca3b
EvgeniyTyupa <boogee2820@gmail.com>
2024-09-26 16:55:53 +0300

    add script in head project

--- src/app/find-car/(components)/FindCarForm.tsx
+++ src/app/find-car/(components)/FindCarForm.tsx
@@ -11,6 +11,9 @@ import {
 import { ButtonPrimary } from '@/shared/Buttons';
 import { findCar } from '@/api/cars';
 import { FindCarProps } from '@/types/car';
+import Modal from '@/shared/Modal';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 const EngineTypesSchema = Yup.object().shape({
   id: Yup.number().required('ID is required'),
@@ -23,6 +26,8 @@ const RequestTimeSchema = Yup.object().shape({
 });
 
 export default function FindCarForm() {
+  const [isSeuccessRequest, setIsSuccessRequest] = useState(false);
+
   const phoneValidationPattern = /\+?[\d]+/;
 
   const engineTypes = [
@@ -112,163 +117,177 @@ export default function FindCarForm() {
     return valuesToRequest;
   }
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        phone: '',
-        brand: '',
-        model: '',
-        color: '',
-        specification: '',
-        additional: '',
-        comment: '',
-        engineType: {
-          id: '',
-          name: ''
-        },
-        requestTime: {
-          id: '',
-          name: ''
-        },
-      }}
-      validationSchema={FindCarSchema}
-      onSubmit={(values, { setSubmitting, resetForm }) => {
-        // trim values
-        const castValues = FindCarSchema.cast(values);
+    <>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          phone: '',
+          brand: '',
+          model: '',
+          color: '',
+          specification: '',
+          additional: '',
+          comment: '',
+          engineType: {
+            id: '',
+            name: ''
+          },
+          requestTime: {
+            id: '',
+            name: ''
+          },
+        }}
+        validationSchema={FindCarSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          // trim values
+          const castValues = FindCarSchema.cast(values);
 
-        const data = returnFindCarDataToRequest(castValues);
-        console.log(data);
-        findCar(data as FindCarProps)
-          .then((res) => {
-            res && resetForm();
-          })
-          .finally(() => setSubmitting(false));
-      }}
-    >
-      {({ errors, touched, isSubmitting }) => (
-        <Form className='grid grid-cols-1 gap-7 -mt-1'>
-          <FormikInput
-            name='name'
-            placeholder='Anthony Rother'
-            title='Full name'
-            rounded='rounded-full'
-            sizeClass='h-14'
-            error={errors.name}
-            touched={touched.name}
-          />
-          {/* ---- */}
-          <FormikInput
-            name='email'
-            type='email'
-            placeholder='some@email.com'
-            title='Email address'
-            rounded='rounded-full'
-            sizeClass='h-14'
-            error={errors.email}
-            touched={touched.email}
-          />
-          {/* ---- */}
-          <FormikPhoneNumberInput
-            title='Phone number'
-            rounded='rounded-full'
-            sizeClass='h-14'
-            error={errors.phone}
-            touched={touched.phone}
-          />
-          {/* ---- */}
-          <FormikInput
-            name='brand'
-            placeholder='Toyota'
-            title='Brand'
-            sizeClass='h-14'
-            rounded='rounded-full'
-            error={errors.brand}
-            touched={touched.brand}
-          />
-          {/* ---- */}
-          <FormikInput
-            name='model'
-            placeholder='Prius'
-            title='Model'
-            sizeClass='h-14'
-            rounded='rounded-full'
-            error={errors.model}
-            touched={touched.model}
-          />
-          {/* ---- */}
-          <FormikInput
-            name='color'
-            placeholder='Black magenta'
-            title='Color'
-            sizeClass='h-14'
-            rounded='rounded-full'
-            error={errors.color}
-            touched={touched.color}
-          />
-          {/* ---- */}
-          <FormikInput
-            name='specification'
-            placeholder='R44'
-            title='Specification'
-            sizeClass='h-14'
-            rounded='rounded-full'
-            error={errors.specification}
-            touched={touched.specification}
-          />
-          {/* ---- */}
-          <FormikTextarea
-            name='additional'
-            placeholder='Enter additional details'
-            title='Additional'
-            rows={3}
-            rounded='rounded-[40px]'
-            error={errors.additional}
-            touched={touched.additional}
-          />
-          {/* ---- */}
-          <FormikTextarea
-            name='comment'
-            placeholder='Enter comment'
-            title='Comment'
-            rows={3}
-            rounded='rounded-[40px]'
-            error={errors.comment}
-            touched={touched.comment}
-          />
-          {/* ---- */}
-          <FormikInputSelector 
-            name='engineType'
-            placeholder='Enter engine type'
-            title='Engine Type'
-            options={engineTypes}
-            rounded='rounded-full'
-            sizeClass='h-14'
-            error={errors.engineType?.name}
-            touched={touched.engineType?.name}
-          />
-          {/* ---- */}
-          <FormikInputSelector 
-            name='requestTime'
-            placeholder='Enter engine type'
-            title='Engine Type'
-            options={requestTime}
-            rounded='rounded-full'
-            sizeClass='h-14'
-            error={errors.requestTime?.name}
-            touched={touched.requestTime?.name}
-          />
+          const data = returnFindCarDataToRequest(castValues);
+          findCar(data as FindCarProps)
+            .then((res) => {
+              res && (resetForm(), setIsSuccessRequest(true));
+            })
+            .finally(() => setSubmitting(false));
+        }}
+      >
+        {({ errors, touched, isSubmitting }) => (
+          <Form className='grid grid-cols-1 gap-7 -mt-1'>
+            <FormikInput
+              name='name'
+              placeholder='Anthony Rother'
+              title='Full name'
+              rounded='rounded-full'
+              sizeClass='h-14'
+              error={errors.name}
+              touched={touched.name}
+            />
+            {/* ---- */}
+            <FormikInput
+              name='email'
+              type='email'
+              placeholder='some@email.com'
+              title='Email address'
+              rounded='rounded-full'
+              sizeClass='h-14'
+              error={errors.email}
+              touched={touched.email}
+            />
+            {/* ---- */}
+            <FormikPhoneNumberInput
+              title='Phone number'
+              rounded='rounded-full'
+              sizeClass='h-14'
+              error={errors.phone}
+              touched={touched.phone}
+            />
+            {/* ---- */}
+            <FormikInput
+              name='brand'
+              placeholder='Toyota'
+              title='Brand'
+              sizeClass='h-14'
+              rounded='rounded-full'
+              error={errors.brand}
+              touched={touched.brand}
+            />
+            {/* ---- */}
+            <FormikInput
+              name='model'
+              placeholder='Prius'
+              title='Model'
+              sizeClass='h-14'
+              rounded='rounded-full'
+              error={errors.model}
+              touched={touched.model}
+            />
+            {/* ---- */}
+            <FormikInput
+              name='color'
+              placeholder='Black magenta'
+              title='Color'
+              sizeClass='h-14'
+              rounded='rounded-full'
+              error={errors.color}
+              touched={touched.color}
+            />
+            {/* ---- */}
+            <FormikInput
+              name='specification'
+              placeholder='R44'
+              title='Specification'
+              sizeClass='h-14'
+              rounded='rounded-full'
+              error={errors.specification}
+              touched={touched.specification}
+            />
+            {/* ---- */}
+            <FormikTextarea
+              name='additional'
+              placeholder='Enter additional details'
+              title='Additional'
+              rows={3}
+              rounded='rounded-[40px]'
+              error={errors.additional}
+              touched={touched.additional}
+            />
+            {/* ---- */}
+            <FormikTextarea
+              name='comment'
+              placeholder='Enter comment'
+              title='Comment'
+              rows={3}
+              rounded='rounded-[40px]'
+              error={errors.comment}
+              touched={touched.comment}
+            />
+            {/* ---- */}
+            <FormikInputSelector 
+              name='engineType'
+              placeholder='Enter engine type'
+              title='Engine Type'
+              options={engineTypes}
+              rounded='rounded-full'
+              sizeClass='h-14'
+              error={errors.engineType?.name}
+              touched={touched.engineType?.name}
+            />
+            {/* ---- */}
+            <FormikInputSelector 
+              name='requestTime'
+              placeholder='Enter request time'
+              title='Request Time'
+              options={requestTime}
+              rounded='rounded-full'
+              sizeClass='h-14'
+              error={errors.requestTime?.name}
+              touched={touched.requestTime?.name}
+            />
 
-          <ButtonPrimary
-            type='submit'
-            disabled={isSubmitting}
-            loading={isSubmitting}
-            className='!text-lg mt-5 w-[102px]'
-          >
-            Send
-          </ButtonPrimary>
-        </Form>
-      )}
-    </Formik>
+            <ButtonPrimary
+              type='submit'
+              disabled={isSubmitting}
+              loading={isSubmitting}
+              className='!text-lg mt-5 w-[102px]'
+            >
+              Send
+            </ButtonPrimary>
+          </Form>
+        )}
+      </Formik>
+        
+      <Modal 
+        title='Thank you!' 
+        isModalOpen={isSeuccessRequest} 
+        setIsModalOpen={setIsSuccessRequest}
+      >
+        <div className='text-center space-y-10'>
+          <InformationCircleIcon className='block mx-auto w-24 h-24 text-yellow-500' />
+          <p className='px-3 text-md font-semibold'>
+            Thank you, your request has been accepted. Soon it will be processed.
+          </p>
+        </div>
+      </Modal>
+    </>
   );
 }
